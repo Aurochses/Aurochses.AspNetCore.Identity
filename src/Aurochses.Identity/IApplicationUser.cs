5f90889f708e57d05ad41b3d43d4732dd09217e9
Vladimir Rodchenko <vladimir@aurochses.com>
2017-09-02 19:05:23 +0300

    main code

---
+++ src/Aurochses.Identity/IApplicationUser.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Aurochses.Identity
+{
+    /// <summary>
+    /// User
+    /// </summary>
+    public interface IApplicationUser
+    {
+        /// <summary>
+        /// Gets or sets the identifier.
+        /// </summary>
+        /// <value>The identifier.</value>
+        Guid Id { get; set; }
+
+        /// <summary>
+        /// First Name
+        /// </summary>
+        /// <value>The first name.</value>
+        string FirstName { get; set; }
+
+        /// <summary>
+        /// Last Name
+        /// </summary>
+        /// <value>The last name.</value>
+        string LastName { get; set; }
+
+        /// <summary>
+        /// Registration date
+        /// </summary>
+        /// <value>The date of user registration.</value>
+        DateTime CreatedDate { get; set; }
+    }
+}
